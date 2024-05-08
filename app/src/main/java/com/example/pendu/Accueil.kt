e143325501e7faf3e93261237e53d22c4c773a1a
itzPhil514 <128484218+itzPhil514@users.noreply.github.com>
2024-05-08 13:47:52 -0400

    finir le layout du jeu et changement de la page accueil vers Jeu

--- app/src/main/java/com/example/pendu/Accueil.kt
+++ app/src/main/java/com/example/pendu/Accueil.kt
@@ -1,11 +1,40 @@
 package com.example.pendu
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Accueil : AppCompatActivity() {
+
+    private lateinit var btn_play: Button
+    private lateinit var btn_history: Button
+    private lateinit var btn_setting: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.accueil)
+
+        // Initialize buttons
+        btn_play = findViewById(R.id.btnPlay)
+        btn_history = findViewById(R.id.btnHistory)
+        btn_setting = findViewById(R.id.btnSetting)
+
+        // Set click listeners
+        btn_play.setOnClickListener {
+            val intent = Intent(this, Jeu::class.java)
+            startActivity(intent)
+        }
+
+        /*btn_history.setOnClickListener {
+            val intent = Intent(this, HistoryActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_setting.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }*/
     }
 }
