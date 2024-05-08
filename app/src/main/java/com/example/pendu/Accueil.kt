520c2bea88638a90f73197c7dfc34395bc518f0f
itzPhil514 <128484218+itzPhil514@users.noreply.github.com>
2024-05-08 11:33:57 -0400

    Classe history et Page accueil

---
+++ app/src/main/java/com/example/pendu/Accueil.kt
@@ -0,0 +1,11 @@
+package com.example.pendu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Accueil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.accueil)
+    }
+}
---
+++ app/src/main/java/com/example/pendu/History.kt
@@ -0,0 +1,3 @@
+package com.example.pendu
+
+class History(var id: Int = 0,var mot: String, var temps: Long, var difficulte: String)
