d80874d4293c786c044fc1284cdc62365e659e56
Alikhan Khakimov <akhakimov@dept-info.crosemont.quebec>
2024-05-18 00:30:52 -0400

    Ajout fonctionnalité getAllDictionnaire et Historique et insertHistorique

--- app/src/main/java/com/example/pendu/Database/DatabaseHelper.kt
+++ app/src/main/java/com/example/pendu/Database/DatabaseHelper.kt
@@ -1,5 +1,6 @@
 package com.example.pendu.Database
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
@@ -15,22 +16,77 @@ class DatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME,
 
         private const val COLUMN_ID = "id"
         private const val COLUMN_MOT = "mot"
-        private const val COLUMN_DIFFICULTE = "difficulté"
+        private const val COLUMN_DIFFICULTÉ = "difficulté"
         private const val COLUMN_LANGAGE = "langage"
         private const val COLUMN_TEMPS = "temps"
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
         val CREATE_TABLE_HISTORIQUE = "CREATE TABLE $TABLE_HISTORIQUE($COLUMN_ID INTEGER PRIMARY KEY," +
-                "$COLUMN_MOT TEXT, $COLUMN_DIFFICULTE TEXT, $COLUMN_TEMPS INTEGER)"
+                "$COLUMN_MOT TEXT, $COLUMN_DIFFICULTÉ TEXT, $COLUMN_TEMPS INTEGER)"
         db?.execSQL(CREATE_TABLE_HISTORIQUE)
 
         val CREATE_TABLE_DICTIONNAIRE = "CREATE TABLE $TABLE_DICTIONNAIRE($COLUMN_ID INTEGER PRIMARY KEY," +
-                "$COLUMN_MOT TEXT, $COLUMN_DIFFICULTE TEXT, $COLUMN_LANGAGE TEXT)"
+                "$COLUMN_MOT TEXT, $COLUMN_DIFFICULTÉ TEXT, $COLUMN_LANGAGE TEXT)"
         db?.execSQL(CREATE_TABLE_DICTIONNAIRE)
     }
 
-    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        TODO("Not yet implemented")
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        val DROP_TABLE_HISTORIQUE = "DROP TABLE IF EXISTS $TABLE_HISTORIQUE"
+        db?.execSQL(DROP_TABLE_HISTORIQUE)
+
+        val DROP_TABLE_DICTIONNAIRE = "DROP TABLE IF EXISTS $TABLE_DICTIONNAIRE"
+        db?.execSQL(DROP_TABLE_DICTIONNAIRE)
+
+         onCreate(db)
+    }
+
+    fun insertHistorique(historique: Historique){
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put(COLUMN_MOT, historique.mot)
+            put(COLUMN_DIFFICULTÉ, historique.difficulté)
+            put(COLUMN_TEMPS, historique.temps)
+        }
+        db.insert(TABLE_HISTORIQUE, null, values)
+        db.close()
+    }
+
+    fun getAllHistorique(): List<Historique>{
+        val historiqueListe = mutableListOf<Historique>()
+        val db = readableDatabase
+        val query = "SELECT * FROM $TABLE_HISTORIQUE"
+        val cursor = db.rawQuery(query, null)
+        while (cursor.moveToNext()){
+            val id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
+            val mot = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_MOT))
+            val difficulté = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_DIFFICULTÉ))
+            val temps = cursor.getLong(cursor.getColumnIndexOrThrow(COLUMN_TEMPS))
+
+            val historique = Historique(id, mot, difficulté, temps)
+            historiqueListe.add(historique)
+        }
+        cursor.close()
+        db.close()
+        return historiqueListe
+    }
+
+    fun getAllDictionnaire(): List<Dictionnaire>{
+        val dictionnaireListe = mutableListOf<Dictionnaire>()
+        val db = readableDatabase
+        val query = "SELECT * FROM $TABLE_DICTIONNAIRE"
+        val cursor = db.rawQuery(query, null)
+        while (cursor.moveToNext()){
+            val id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
+            val mot = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_MOT))
+            val difficulté = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_DIFFICULTÉ))
+            val langage = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_LANGAGE))
+
+            val dictionnaire = Dictionnaire(id, mot, difficulté, langage)
+            dictionnaireListe.add(dictionnaire)
+        }
+        cursor.close()
+        db.close()
+        return dictionnaireListe
     }
 }
